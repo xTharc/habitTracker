ca564f01ce78f6175e35970e7a382ae972bd4a57
xTharc <Tharc@users.noreply.github.com>
2020-04-09 13:23:12 +0200

    I added authentication V1 :wq

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const socket = require("socket.io");
 const _ = require("lodash");
+const session = require('express-session');
+const passport = require('passport');
+var passportLocalMongoose = require("passport-local-mongoose");
+
+
 const app = express();
 const ejsLint = require('ejs-lint');
 app.set('view engine', 'ejs');
@@ -12,12 +17,38 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// important place above mongo connection and after use passport
+app.use(session({
+  secret: "ourLittleSecret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/habitDB", {useNewUrlParser: true});
+mongoose.set("useCreateIndex",true);
 // ----------------------------------MONGODB SCHEMAS AND MODULES----------------
 function daysInMonth (month, year) {
     return new Date(year, month, 0).getDate();
 }
 
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+// Red Je pomemben !!
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const userLayouts = {
   habitName: [String],
@@ -50,9 +81,58 @@ console.log(day,month,year,daysinThisMonth,daysinNextMonth);
 
 //-------------------------------------OUR APP----------------------------------
 var foundLayout;
-var foundLayoutnames=[];
 var checker = 1;
+
 app.get("/", function(req,res){
+  if (req.isAuthenticated()){
+    res.redirect("/home");
+  } else {
+    res.render("home",{});
+  }
+});
+
+app.post("/login", function(req,res){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, function (err){
+    if(err){
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/home");
+      });
+    }
+  })
+
+});
+
+app.get("/register", function(req,res){
+
+res.render("register");
+});
+
+app.post("/register", function(req,res){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    console.log("i get here tho ?")
+    if(err){
+      console.log("i in error");
+      console.log(err);
+    } else {
+        console.log("i in no error guuud stuff");
+      passport.authenticate("local")(req,res, function(){
+        res.redirect("/home");
+      });
+    }
+  });
+});
+
+
+
+app.get("/home", function(req,res){
+
+if (req.isAuthenticated()){
 
   userLay.find({}, function(err, foundLayouts){
   if (err){
@@ -64,19 +144,19 @@ app.get("/", function(req,res){
       console.log(foundLayout.length);
       var dolzina = foundLayout.length;
       for (var i=0;i< dolzina ;i++){
-      foundLayoutnames[i] = foundLayout[i].habitName;
-      console.log(foundLayoutnames[i]);
       };
-      // console.log(foundLayoutnames.length);
     } else {
       console.log("Ne jebe pol posto");
     }
   }
-  res.render("home",{day: day,dinThisMonth: daysinThisMonth,dinNextMonth:daysinNextMonth,foundLayout: foundLayout});
-
+  res.render("index",{day: day,dinThisMonth: daysinThisMonth,dinNextMonth:daysinNextMonth,foundLayout: foundLayout});
+console.log(foundLayout);
 });
-console.log(foundLayoutnames);
 
+
+} else {
+  res.redirect("/");
+}
 
 });
 
@@ -89,7 +169,7 @@ console.log("TESTing  ->"+req.body.habitName+"  is" +req.body.habitData);
 });
 
 
-app.post("/", function(req,res){
+app.post("/home", function(req,res){
   var adding = req.body.habit;
   console.log(adding);
 
@@ -101,12 +181,10 @@ app.post("/", function(req,res){
 
   res.redirect("/");
 });
-app.get("/login", function(req,res){
-res.render("home");
-});
-app.get("/register", function(req,res){
-res.render("home");
-});
+// app.get("/login", function(req,res){
+// res.render("home");
+// });
+
 
 
 
